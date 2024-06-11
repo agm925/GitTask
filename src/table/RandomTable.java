e03eafe7dbfb716ee9c9cd1b74fde970bfbd2afa
GomezMolinaAlejandro <alx6950@gmail.com>
2024-06-11 18:57:27 +0200

    Revert "Sorted table"
    This reverts commit dc3b6029a0fdbb529f871fbcc22d9b475a632e1c.

--- src/table/RandomTable.java
+++ src/table/RandomTable.java
@@ -1,43 +1,33 @@
 package table;
 import java.util.Random;
-import java.util.Arrays;
-import java.util.Comparator;
 public class RandomTable {
 	public static void main(String[] args) {
 		
         String[] encabezados = {"ID", "Nombre", "Edad"};
         
-        
+       
         String[] nombres = {"Juan", "Ana", "Luis", "Carlos", "Maria"};
         
         
         Random random = new Random();
         
-        
+       
         int numFilas = 5;
         
         
-        String[][] tabla = new String[numFilas][3];
+        String[][] tabla = new String[numFilas + 1][3];
         
-      
-        for (int i = 0; i < numFilas; i++) {
-            tabla[i][0] = String.valueOf(i + 1); // ID
+        
+        for (int i = 0; i < encabezados.length; i++) {
+            tabla[0][i] = encabezados[i];
+        }
+        
+        for (int i = 1; i <= numFilas; i++) {
+            tabla[i][0] = String.valueOf(i); // ID
             tabla[i][1] = nombres[random.nextInt(nombres.length)]; 
             tabla[i][2] = String.valueOf(random.nextInt(50) + 18); 
         }
         
-        
-        Arrays.sort(tabla, new Comparator<String[]>() {
-            @Override
-            public int compare(String[] fila1, String[] fila2) {
-                return Integer.compare(Integer.parseInt(fila1[2]), Integer.parseInt(fila2[2]));
-            }
-        });
-        
-        for (String encabezado : encabezados) {
-            System.out.print(encabezado + "\t");
-        }
-        System.out.println();
         
         for (int i = 0; i < tabla.length; i++) {
             for (int j = 0; j < tabla[i].length; j++) {

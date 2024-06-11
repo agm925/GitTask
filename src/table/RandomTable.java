aaa20155bfea92e48120d3097f189440c4a28e83
GomezMolinaAlejandro <alx6950@gmail.com>
2024-06-11 19:21:45 +0200

    cambio en rama

--- src/table/RandomTable.java
+++ src/table/RandomTable.java
@@ -1,34 +1,51 @@
 package table;
 import java.util.Random;
+import java.util.Arrays;
+import java.util.Comparator;
 public class RandomTable {
 	public static void main(String[] args) {
 		
-        String[] encabezados = {"ID", "Nombre", "Edad"};
+		 // Encabezados de la tabla
+        String[] encabezados = {"ID", "Nombre", "Edad", "Ciudad"};
         
-       
+        // Nombres para la columna "Nombre"
         String[] nombres = {"Juan", "Ana", "Luis", "Carlos", "Maria"};
         
+        // Ciudades para la columna "Ciudad"
+        String[] ciudades = {"Madrid", "Barcelona", "Valencia", "Sevilla", "Bilbao"};
         
+        // Crear un objeto Random para generar valores aleatorios
         Random random = new Random();
         
-       
+        // Número de filas de la tabla (excluyendo los encabezados)
         int numFilas = 5;
         
+        // Matriz para almacenar la tabla
+        String[][] tabla = new String[numFilas][4];
         
-        String[][] tabla = new String[numFilas + 1][3];
-        
-        
-        for (int i = 0; i < encabezados.length; i++) {
-            tabla[0][i] = encabezados[i];
+        // Llenar las filas con datos aleatorios
+        for (int i = 0; i < numFilas; i++) {
+            tabla[i][0] = String.valueOf(i + 1); // ID
+            tabla[i][1] = nombres[random.nextInt(nombres.length)]; // Nombre aleatorio
+            tabla[i][2] = String.valueOf(random.nextInt(50) + 18); // Edad aleatoria entre 18 y 67
+            tabla[i][3] = ciudades[random.nextInt(ciudades.length)]; // Ciudad aleatoria
         }
         
-        for (int i = 1; i <= numFilas; i++) {
-            tabla[i][0] = String.valueOf(i); // ID
-            tabla[i][1] = nombres[random.nextInt(nombres.length)]; 
-            tabla[i][2] = String.valueOf(random.nextInt(50) + 18); 
+       
+        Arrays.sort(tabla, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] fila1, String[] fila2) {
+                return fila1[1].compareTo(fila2[1]); 
+            }
+        });
+        
+        
+        for (String encabezado : encabezados) {
+            System.out.print(encabezado + "\t");
         }
+        System.out.println();
         
-        
+        /
         for (int i = 0; i < tabla.length; i++) {
             for (int j = 0; j < tabla[i].length; j++) {
                 System.out.print(tabla[i][j] + "\t");
